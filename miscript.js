333faa964581ca91dde415b86fc84781352e9243
Azucena <61836749+azucenaGV@users.noreply.github.com>
2020-05-07 10:56:04 -0500

    Add files via upload

--- miscript.js
+++ miscript.js
@@ -1,2 +1,5 @@
-document.write('<h1>Hola mundo </h1>');
-alert('hola usuario');//manda un mensaje mostrando una pantalla en el navegador//
+var nombre= 'Azucena';
+var apellido= 'Gomez';
+
+document.write('variable nombre: ' +nombre);
+document.write('variable apellido:' +apellido);
